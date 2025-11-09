651f6183fa7299a1ff1cd1b7d0a0c855bda53ead
ST4T1K <mjobrien06@hotmail.com>
2025-11-10 00:21:36 +0800

    feat(wip): user interface

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import PIL
 import lz4
 import lzf
+from pyexpat.errors import messages
+
 
 def find_pkgs(pkg_list: dict[str: Path], directory: Path) -> dict[str:Path]:
     if directory.is_dir():
@@ -28,11 +30,61 @@ def move_directory(file: tuple[str, Path], output_dir: Path = r"C:\Users\Maddie\
     print(f"moving extracted files: {file[1]} to output directory")
     shutil.move(Path(extract[:-4]), Path(output_dir, file[0]))
 
-output_directory = Path(r"C:\Users\Maddie\source\repos\HadesIISpriteExtractor\output")
-packages_directory = Path(r"C:\Users\Maddie\source\repos\HadesIISpriteExtractor\pkg_test")
+def read_config(config_file):
+    print("reading config...")
+    with open(r"./config.txt") as config:
+        settings = config.readlines()
+        if len(settings) == 0:
+            print("No settings found")
+        print(f"settings found: \noutput dir = {settings[0]}pkg dir = {settings[1]}")
+        return Path(settings[0].strip()), Path(settings[1].strip())
+
+# output_directory = Path(r"C:\Users\Maddie\source\repos\HadesIISpriteExtractor\output")
+# packages_directory = Path(r"C:\Users\Maddie\source\repos\HadesIISpriteExtractor\pkg_test")
+#
+#
+# file_paths = find_pkgs({}, packages_directory)
+#
+# for file_tup in file_paths.items():
+#     run_command(file_tup)
+
+def get_and_validate_input(message: str, current: Path = None):
+    valid = False
+    while not valid:
+        if current:
+            user_input = input(f"{message}{current!r})\n>>>").strip()
+        else:
+            user_input = input(f"{message}\n>>>").strip()
+        try:
+            Path(user_input)
+        except Exception as e:
+            print("unable to convert input to path")
+        if Path(user_input).is_dir():
+            valid = True
+            continue
+        return Path(user_input)
 
 
-file_paths = find_pkgs({}, packages_directory)
+def main():
+    running = True
 
-for file_tup in file_paths.items():
-    run_command(file_tup)
+    output_directory = Path("./output").absolute()
+    packages_directory = ""
+
+    print(output_directory)
+
+    while running:
+        if Path("./config.txt").is_file():
+            output_directory, packages_directory = read_config(r"./config.txt")
+        else:
+            print("no config file found")
+
+        
+        output_directory = get_and_validate_input("enter an output directory\n(current: ", output_directory)
+
+        packages_directory = get_and_validate_input(r"enter a package directory (e.g. C:\Program Files (x86)\Steam\steamapps\common\Hades II\Content\Packages", packages_directory)
+
+        running = False
+
+if __name__ == "__main__":
+    main()
