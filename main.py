0de2f6821c471340ddfd8195e7aecdd498769442
ST4T1K <mjobrien06@hotmail.com>
2025-11-10 23:40:58 +0800

    feat: v1.0

--- main.py
+++ main.py
@@ -22,7 +22,10 @@ def find_pkgs(pkg_list: dict[str: Path], directory: Path) -> dict[str:Path]:
 
 def run_command(file: tuple[str, Path], output_directory: Path):
     command = f'deppth ex "{file[1]}"'
-    subprocess.run(command)
+    try:
+        subprocess.run(command)
+    except Exception as e:
+        print(f"Err: command could not be run: {e}")
     move_directory(file, output_directory)
 
 def move_directory(file: tuple[str, Path], output_dir: Path = r"C:\Users\Maddie\source\repos\HadesIISpriteExtractor\output"):
@@ -36,55 +39,101 @@ def read_config(config_file):
         settings = config.readlines()
         if len(settings) == 0:
             print("No settings found")
-        print(f"settings found: \noutput dir = {settings[0]}pkg dir = {settings[1]}")
-        return Path(settings[0].strip()), Path(settings[1].strip())
-
-# output_directory = Path(r"C:\Users\Maddie\source\repos\HadesIISpriteExtractor\output")
-# packages_directory = Path(r"C:\Users\Maddie\source\repos\HadesIISpriteExtractor\pkg_test")
-#
-#
-# file_paths = find_pkgs({}, packages_directory)
-#
-# for file_tup in file_paths.items():
-#     run_command(file_tup)
+            return
+        print(f"settings found")
+        output_path = Path(settings[0].strip()).absolute()
+        return output_path
 
 def get_and_validate_input(message: str, current: Path = None):
     valid = False
     while not valid:
         if current:
-            user_input = input(f"{message}{current!r})\n>>>").strip()
+            user_input = input(f"{message}{current!r})\n>>> ").strip()
         else:
-            user_input = input(f"{message}\n>>>").strip()
+            user_input = input(f"{message}\n>>> ").strip()
+
         try:
+            user_input = r"{}".format(user_input)
             Path(user_input)
         except Exception as e:
             print("unable to convert input to path")
+
         if Path(user_input).is_dir():
             valid = True
-            continue
-        return Path(user_input)
+            return Path(user_input)
+        else:
+            print("path is not valid")
+
 
 
 def main():
     running = True
 
-    output_directory = Path("./output").absolute()
+    output_directory = ""
     packages_directory = ""
 
     print(output_directory)
 
     while running:
         if Path("./config.txt").is_file():
-            output_directory, packages_directory = read_config(r"./config.txt")
+            output_directory = read_config(r"./config.txt") or Path("./output").absolute()
         else:
             print("no config file found")
 
-        
-        output_directory = get_and_validate_input("enter an output directory\n(current: ", output_directory)
+        dirs_valid = False
+        while not dirs_valid:
+            u_input = input(f"enter output directory? current: {output_directory}\n[y/n] >>> ").lower()
+            if u_input == 'y':
+                output_directory = None
+                while not output_directory:
+                    # bandaid fix
+                    user_input = input(f"enter an output directory\n(current: {output_directory})\n>>> ")
+                    user_input = r"{}".format(user_input)
+                    try:
+                        user_input = Path(user_input)
+                    except:
+                        pass
 
-        packages_directory = get_and_validate_input(r"enter a package directory (e.g. C:\Program Files (x86)\Steam\steamapps\common\Hades II\Content\Packages", packages_directory)
+                    if isinstance(user_input, Path):
+                        output_directory = user_input
 
-        running = False
+            while not packages_directory:
+                packages_directory = get_and_validate_input(r"enter a package directory (e.g. C:....\Program Files (x86)\Steam\steamapps\common\Hades II\Content\Packages", packages_directory)
+
+
+            # # possibly unneeded
+            # if output_directory.is_dir() and packages_directory.is_dir():
+            #     dirs_valid = True
+            # elif not output_directory.is_dir() and  packages_directory.is_dir():
+            #     print("Err: output directory is not valid")
+            # elif output_directory.is_dir() and not packages_directory.is_dir():
+            #     print("Err: packages directory is not valid")
+            # else:
+            #     print("Err: output directory and packages directory is not valid")
+
+            file_paths = {}
+            file_paths = find_pkgs(file_paths, packages_directory)
+
+            if not file_paths:
+                print("Err: no valid (.pkg) files found")
+            else:
+                for file_tup in file_paths.items():
+                    run_command(file_tup, output_directory)
+
+            print("Extraction complete")
+
+            u_input = input(f"extract more packages?\n[y/n] >>> ").lower()
+            # NOTE: not  working ???
+            if u_input != 'y':
+                running = False
+    # NOTE: not  working ???
+    with open("./config.txt", 'w') as file:
+        print("writing output dir to file...")
+        file.truncate()
+        file.write(str(output_directory))
+
+    print("quitting...")
+    quit(0)
 
 if __name__ == "__main__":
     main()
